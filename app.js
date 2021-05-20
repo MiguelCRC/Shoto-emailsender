2e204c923f1c340dc33026d9a415bd7711dbc1ed
Miguel CRC <menriquez@crc.global>
2021-05-20 08:22:33 -0600

    initial structure for Express,Postgres,Sequelize project

---
+++ app.js
@@ -0,0 +1,43 @@
+let createError = require('http-errors');
+let express = require('express');
+let cors = require('cors');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+
+let app = express();
+app.use(cors());
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(request, response, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(error, request, response, next) {
+  // set locals, only providing error in development
+  response.locals.message = error.message;
+  response.locals.error = request.app.get('env') === 'development' ? error : {};
+
+  // render the error page
+  response.status(error.status || 500);
+  response.render('error');
+});
+
+module.exports = app;
---
+++ compose/local/api/start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export DEBUG=emailsenderapi:*
+/app/node_modules/.bin/nodemon -L ./bin/www
